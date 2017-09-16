ac6f94a92098b1924ca186f36cc4a7d923e8bfed
Keith Burnell <keith@kburnell.com>
2017-09-16 09:08:29 -0500

    moved action type magic strings out to constants, wrapped up 'Redux Flow' module

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,8 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
 	//debugger; //1st
-	return {type: 'CREATE_COURSE', course};
+	return {type: types.CREATE_COURSE, course};
+
+
 }
